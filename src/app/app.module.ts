f92db34d80bdf2f167e91d3fea5d079a53143a88
Brodie Pestell <brodie.pestell7@gmail.com>
2024-08-13 19:39:09 +0100

    Remove Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,8 @@ import { EventFrequencyFormComponent } from './components/event-frequency-form/e
 import {FormsModule} from "@angular/forms";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {MatButtonModule} from "@angular/material/button";
 import { CreateEventComponent } from './components/create-event/create-event.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
-import {MatSelectModule} from "@angular/material/select";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatChipsModule} from "@angular/material/chips";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatCheckboxModule} from "@angular/material/checkbox";
 import { EventQuickListComponent } from './components/event-quick-list/event-quick-list.component';
-import {MatStepperModule} from "@angular/material/stepper";
 import {Button} from "primeng/button";
 import {StepperModule} from "primeng/stepper";
 import {FloatLabelModule} from "primeng/floatlabel";
@@ -46,17 +35,6 @@ import {RadioButtonModule} from "primeng/radiobutton";
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatExpansionModule,
-    MatChipsModule,
-    MatRadioModule,
-    MatCheckboxModule,
-    MatStepperModule,
     Button,
     StepperModule,
     FloatLabelModule,
