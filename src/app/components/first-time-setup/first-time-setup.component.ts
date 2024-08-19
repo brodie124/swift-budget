a1fb4c1140ef6ed3601d5f44e45467f48a82022b
Brodie Pestell <brodie.pestell7@gmail.com>
2024-08-19 20:05:33 +0100

    Update look & feel of the "First time setup" modal

--- src/app/components/first-time-setup/first-time-setup.component.ts
+++ src/app/components/first-time-setup/first-time-setup.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {DialogModule} from "primeng/dialog";
 import {InputSwitchModule} from "primeng/inputswitch";
-import {FormsModule} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {InputTextModule} from "primeng/inputtext";
 import {Button} from "primeng/button";
 import {PasswordModule} from "primeng/password";
@@ -15,14 +15,45 @@ import {PasswordModule} from "primeng/password";
     FormsModule,
     InputTextModule,
     Button,
-    PasswordModule
+    PasswordModule,
+    ReactiveFormsModule
   ],
   templateUrl: './first-time-setup.component.html',
   styleUrl: './first-time-setup.component.less'
 })
 export class FirstTimeSetupComponent {
   public isVisible: boolean = true;
+  public hasSubmitted: boolean = false;
 
-  public enableEncryption: boolean = false;
-  public encryptionKey: string = '';
+  public readonly firstTimeForm: FormGroup;
+
+  public get enableEncryption(): boolean {
+    return !!this.enableEncryptionControl.value;
+  }
+
+  public get enableEncryptionControl(): AbstractControl {
+    return this.firstTimeForm.controls['enableEncryption'];
+  }
+
+  public get masterPasswordControl(): AbstractControl {
+    return this.firstTimeForm.controls['masterPassword'];
+  }
+
+  constructor(formBuilder: FormBuilder) {
+    this.firstTimeForm = formBuilder.group({
+      enableEncryption: [true, Validators.required],
+      masterPassword: ['', Validators.required]
+    });
+  }
+
+  public saveAndExit(): void {
+    this.masterPasswordControl.setValidators(this.enableEncryption ? Validators.required : null);
+    this.masterPasswordControl.updateValueAndValidity();
+
+    this.hasSubmitted = true;
+    if(!this.firstTimeForm.valid)
+      return;
+
+    this.isVisible = false;
+  }
 }
