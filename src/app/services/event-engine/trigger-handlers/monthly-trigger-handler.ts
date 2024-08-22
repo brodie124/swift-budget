99035986574a2f4e452fe8706525ba8752455226
Brodie Pestell <brodie.pestell7@gmail.com>
2024-08-23 00:16:45 +0100

    Correctly account for the date needing to be pushed back or forward

--- src/app/services/event-engine/trigger-handlers/monthly-trigger-handler.ts
+++ src/app/services/event-engine/trigger-handlers/monthly-trigger-handler.ts
@@ -1,13 +1,15 @@
 import {Injectable} from "@angular/core";
-import {EventTriggerMonthly} from "../../../types/event/event";
+import {EventTriggerAdvancedOptions, EventTriggerMonthly, InvalidDayFallback} from "../../../types/event/event";
 import moment from "moment/moment";
 import {EventTriggerHandler} from "./event-trigger-handle";
 
+export type EventTriggerMonthlyAdvanced = EventTriggerMonthly & EventTriggerAdvancedOptions;
+
 @Injectable({ providedIn: 'root'})
-export class MonthlyTriggerHandler implements EventTriggerHandler<EventTriggerMonthly> {
+export class MonthlyTriggerHandler implements EventTriggerHandler<EventTriggerMonthlyAdvanced> {
 
   public calculateOccurrences(
-    trigger: EventTriggerMonthly,
+    trigger: EventTriggerMonthlyAdvanced,
     startDate: moment.Moment,
     endDate: moment.Moment
   ): Array<moment.Moment> {
@@ -17,18 +19,25 @@ export class MonthlyTriggerHandler implements EventTriggerHandler<EventTriggerMo
     for (let i = 0; i <= deltaMonths; i++) {
       switch (trigger.options.type) {
         case 'specific-date':
-          // TODO: we assume the next occurrence will always be in the following month, but that is not always be the case
-          // A start date of 27/03 would be the 28th day bill onto 28/04
-          const incrementedDate = moment(startDate).add(i, 'months');
-          const year = incrementedDate.year();
-          const month = incrementedDate.month() + 1; // .month() returns the index of the month
-          const day = trigger.options.dayOfMonth;
+          let evaluationDate = moment(startDate)
+            .add(i, 'months');
 
-          // TODO: we need to handle when `dayOfMonth` is greater than the number of days in the month (it should be pushed back to the next month)
-          const dateString = `${year}-${month}-${day}`;
-          const date = moment.utc(dateString, 'YYYY-MM-DD')
-          if (date.isAfter(startDate) && date.isBefore(endDate))
-            occurrences.push(date);
+
+          if(trigger.options.dayOfMonth <= evaluationDate.daysInMonth()) {
+            // Use the date as specified
+            evaluationDate = evaluationDate.set('date', trigger.options.dayOfMonth);
+          } else {
+            if(trigger.advancedOptions.invalidDayFallback === InvalidDayFallback.NextAllowedDay) {
+              // Go to the first day of the following month
+              evaluationDate = evaluationDate.add(1, 'month').set('date', 1);
+            } else {
+              // Go to the last available day of the same month
+              evaluationDate = evaluationDate.set('date', evaluationDate.daysInMonth());
+            }
+          }
+
+          if (evaluationDate.isSameOrAfter(startDate) && evaluationDate.isSameOrBefore(endDate))
+            occurrences.push(evaluationDate);
       }
     }
 
