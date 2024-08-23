bc3aa88f30afbfe7005cedd790f10fec98df10c7
Brodie <brodie.pestell7@gmail.com>
2024-08-23 20:15:51 +0100

    Move responsibility of calculating event occurrences into the dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
-import {Component, effect, inject, OnDestroy, OnInit, signal} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
 import {EventManagerService} from "../../services/event-manager.service";
-import {FinancialEvent} from "../../types/financial/financial-event";
+import {FinancialEvent, FinancialEventOccurrence} from "../../types/financial/financial-event";
 import {
   EventQuickListDateRange
 } from "../event-quick-list/subcomponents/event-quick-list-toolbar/event-quick-list-toolbar.component";
 import {getMomentUtc} from "../../utils/moment-utils";
 import {Subscription} from "rxjs";
+import {FinancialEventService} from "../../services/financial-event.service";
+import {FinancialEventHistoryManager} from "../../services/financial-event-history-manager.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -14,22 +16,43 @@ import {Subscription} from "rxjs";
 })
 export class DashboardComponent implements OnInit, OnDestroy {
   private readonly _eventManager: EventManagerService = inject(EventManagerService);
+  private readonly _financialEventService: FinancialEventService = inject(FinancialEventService);
+  private readonly _financialEventHistoryManager: FinancialEventHistoryManager = inject(FinancialEventHistoryManager);
   private readonly _subscriptions: Subscription = new Subscription();
 
   public events = signal<ReadonlyArray<FinancialEvent>>([]);
+  public occurrences = signal<ReadonlyArray<FinancialEventOccurrence>>([]);
+
   public quickListDateRange = signal<EventQuickListDateRange>({
     startDate: getMomentUtc(),
     endDate: getMomentUtc().add(1, 'month')
   });
 
+
+
   public async ngOnInit() {
     await this._eventManager.loadAsync();
     this._subscriptions.add(this._eventManager.events$.subscribe(events => {
       this.events.set(events);
+      this.calculateOccurrences(this.events());
+    }));
+
+    this._subscriptions.add(this._financialEventHistoryManager.historyChanged$.subscribe(() => {
+      this.calculateOccurrences(this.events());
     }));
   }
 
   public ngOnDestroy() {
     this._subscriptions.unsubscribe();
+  }
+
+  private async calculateOccurrences(events: ReadonlyArray<FinancialEvent>) {
+    const eventOccurrences = await this._financialEventService.getCalculatedEventsAsync(
+      events,
+      this.quickListDateRange().startDate,
+      this.quickListDateRange().endDate
+    );
+
+    this.occurrences.set(eventOccurrences);
   }
 }
--- src/app/components/event-quick-list/event-quick-list.component.ts
+++ src/app/components/event-quick-list/event-quick-list.component.ts
@@ -1,58 +1,27 @@
-import {Component, computed, inject, input, OnDestroy, OnInit, signal, Signal} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {FinancialEvent, FinancialEventHistory, FinancialEventOccurrence} from "../../types/financial/financial-event";
 import moment from "moment";
 import {compareMomentsAscending} from "../../helpers/moment-utils";
-import {FinancialEventService} from "../../services/financial-event.service";
 import {getMomentUtc} from "../../utils/moment-utils";
-import {toObservable} from "@angular/core/rxjs-interop";
-import {from, map, Subscription, switchMap} from "rxjs";
-import {FinancialEventHistoryManager} from "../../services/financial-event-history-manager.service";
 
 @Component({
   selector: 'app-event-quick-list',
   templateUrl: './event-quick-list.component.html',
   styleUrls: ['./event-quick-list.component.less']
 })
-export class EventQuickListComponent implements OnInit, OnDestroy {
-  private readonly _financialEventService: FinancialEventService = inject(FinancialEventService);
-  private readonly _financialEventHistoryService: FinancialEventHistoryManager = inject(FinancialEventHistoryManager);
-  private readonly _subscriptions: Subscription = new Subscription();
-  public readonly triggerRecompute = signal<boolean>(false);
-
-  public readonly events = input.required<ReadonlyArray<FinancialEvent>>();
+export class EventQuickListComponent {
+  public readonly occurrences = input.required<ReadonlyArray<FinancialEventOccurrence>>();
   public readonly startDate = input.required<moment.Moment>();
   public readonly endDate = input.required<moment.Moment>();
 
-  public itemsPromise = computed(() => {
-    this.triggerRecompute(); // Here as a trigger
-    return this._financialEventService.getCalculatedEventsAsync(this.events(), this.startDate(), this.endDate())
-  });
-
-  public items$ = toObservable(this.itemsPromise).pipe(
-    switchMap(e => from(e)), map(calculatedEvents => calculatedEvents
-      .map(this.createQuickListItem)
-      .sort((a: any, b: any) => compareMomentsAscending(a.nextOccurrence.date, b.nextOccurrence.date))
-    ));
-
-
-  public ngOnInit(): void {
-    this._subscriptions.add(
-      this._financialEventHistoryService.historyChanged$.subscribe(
-        () => this.triggerRecompute.set(!this.triggerRecompute())
-      )
-    );
-  }
-
-  public ngOnDestroy(): void {
-    this._subscriptions.unsubscribe();
-  }
-
+  public items = computed(() => this.occurrences()
+    .map(occurrence => this.createQuickListItem(occurrence))
+    .sort((a, b) => compareMomentsAscending(a.nextOccurrence.date, b.nextOccurrence.date)));
 
   private createQuickListItem(calculatedEvent: FinancialEventOccurrence): EventQuickListItem {
     const comparisonDate = getMomentUtc();
     const timeUntilSeconds = calculatedEvent.date.diff(comparisonDate, 'seconds', false);
     const timeUntilDays = calculatedEvent.date.diff(comparisonDate, 'days', false);
-    // const absed = Math.abs(timeUntilSeconds);
 
     return {
       history: calculatedEvent.history,
@@ -64,8 +33,6 @@ export class EventQuickListComponent implements OnInit, OnDestroy {
         timeUntil: {
           seconds: timeUntilSeconds,
           days: timeUntilDays
-          // seconds: timeUntilSeconds,
-          // days:  Math.floor(timeUntilSeconds / 86400)
         }
       }
     }
