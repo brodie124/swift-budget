0ea81e54f780cfff86dc16f456862105fc1211b8
Brodie <brodie.pestell7@gmail.com>
2024-08-24 16:31:32 +0100

    Add animations to quick list items

--- src/app/components/event-quick-list/event-quick-list.component.ts
+++ src/app/components/event-quick-list/event-quick-list.component.ts
@@ -3,11 +3,24 @@ import {FinancialEvent, FinancialEventHistory, FinancialEventOccurrence} from ".
 import moment from "moment";
 import {compareMomentsAscending} from "../../helpers/moment-utils";
 import {getMomentUtc} from "../../utils/moment-utils";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-event-quick-list',
   templateUrl: './event-quick-list.component.html',
-  styleUrls: ['./event-quick-list.component.less']
+  styleUrls: ['./event-quick-list.component.less'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({opacity: 0, height: '0'}),
+        animate('100ms ease-in', style({opacity: 1, height: '*'})), //100ms
+      ]),
+      transition(':leave', [
+        style({opacity: 1, height: '*'}),
+        animate('100ms ease-in', style({opacity: 0, height: '0'})), //100ms
+      ])
+    ])
+  ]
 })
 export class EventQuickListComponent {
   public readonly occurrences = input.required<ReadonlyArray<FinancialEventOccurrence>>();
