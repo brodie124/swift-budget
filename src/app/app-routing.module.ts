a26008ef8cd214d8f412d979441430828c64f756
Brodie <brodie.pestell7@gmail.com>
2024-08-24 15:31:49 +0100

    Remove unused route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
-import {CreateEventModalComponent} from "./components/event-modals/create-event-modal/create-event-modal.component";
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: DashboardComponent,
-  },
-  {
-    path: 'create',
-    pathMatch: 'full',
-    component: CreateEventModalComponent
   }
 ];
 
