e3698e96395500cc870bd888fc59041366d0ec4c
Brodie Pestell <brodie.pestell7@gmail.com>
2024-08-26 15:31:42 +0100

    Add layer of abstraction to local storage for purposes of monitoring/notifying

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,28 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {filter, Observable, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService implements Storage {
-    // [name: string]: any;
+  private _listeners: Map<string, () => {}> = new Map();
 
-    public get length(): number {
-      return localStorage.length;
-    }
+  private readonly _getSubject: Subject<{ key: string }> = new Subject();
+  private readonly _setSubject: Subject<{ key: string, value: string | null }> = new Subject();
+  private readonly _removeSubject: Subject<{ key: string }> = new Subject();
 
-    clear(): void {
-        return localStorage.clear();
-    }
-    getItem(key: string): string | null {
-        return localStorage.getItem(key);
-    }
-    key(index: number): string | null {
-        return localStorage.key(index);
-    }
-    removeItem(key: string): void {
-        return localStorage.removeItem(key);
-    }
-    setItem(key: string, value: string): void {
-        return localStorage.setItem(key, value);
-    }
+  public readonly get$: Observable<{ key: string }> = this._getSubject.asObservable();
+  public readonly set$: Observable<{ key: string, value: string | null }> = this._setSubject.asObservable();
+  public readonly remove$: Observable<{ key: string }> = this._removeSubject.asObservable();
+
+  public get length(): number {
+    return localStorage.length;
+  }
+
+  clear(): void {
+    return localStorage.clear();
+  }
+
+  getItem(key: string): string | null {
+    const value = localStorage.getItem(key);
+    this._getSubject.next({ key });
+    return value;
+  }
+
+  key(index: number): string | null {
+    return localStorage.key(index);
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+    this._removeSubject.next({key});
+  }
+
+  setItem(key: string, value: string): void {
+    localStorage.setItem(key, value);
+    this._setSubject.next({key, value});
+  }
 }
+
